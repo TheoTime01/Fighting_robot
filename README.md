a0fc0989fe29c6de66ee37cbad0dc9b1ef196e0f
TheotimeP <perricht@gmail.com>
2023-02-08 15:23:35 +0100

    2.0

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 # Fighting_robot
 
-références: https://github.com/Lynxmotion/Arduino-PS2X
-https://www.lextronic.fr/gamepad-sans-fil-pour-arduino-39569.html
+références: 
+
+- https://github.com/Lynxmotion/Arduino-PS2X
+
+https://joy-it.net/en/products/SBC-WLGamepad
+
+
+## Description du projet
+
+Dans le cadre d'ouverture à la robotique pour les nouveaux membres de mon association étudiante Galiléo, j'ai décidé de développer un robot de combat qui se déplace et renverse ses adversaires. 
+Il est composé de 2 robots, chacun équipé d'un gamepad sans fil. Le robot est équipé de 2 moteurs, d'un module de réception, d'un arduino et d'une batterie 9V. 
+Le robot est contrôlé par un gamepad sans fil.
+
+## Matériel
+- 2 moteurs DC 
+- 1 servo-moteur 
+- 1 module de réception
+- gamepad sans fil
+- Arduino Uno
+- batterie 9V
+- pièces détachées
+- LED
+- driver
+
+## Logiciel
+- Arduino IDE
+
+## Montage
+
+
+## Contrôle du robot
+![image](images/mannette.jpg)
+
+## Montage des composants
+
+
+
+
+
+
+
--- robot combat/robot combat.ino
+++ robot combat/robot combat.ino
@@ -1,11 +1,21 @@
+
+/*
+author : theotime Perrichet
+date : 2019-05-06
+description : robot combat
+*/
+
+ps2x.config_gamepad(9,7,6,8, true, true)
+
+//bibliothèque
 #include <PS2X_lib.h>
 #include <Servo.h>
 
 // PS2 Controller Connections
-#define PS2_DAT    A1
-#define PS2_CMD    A0
-#define PS2_SEL    A2
-#define PS2_CLK    A3
+#define PS2_DAT    13
+#define PS2_CMD    11
+#define PS2_SEL    10
+#define PS2_CLK    12
 
 // Motor Connections
 #define M1 6
@@ -13,6 +23,9 @@
 
 // Servo Pin
 #define SERVO_PIN 9
+
+//LED Pin
+#define LED_PIN 2
 
 // Speed of Motors
 int leftspeed = 150;
@@ -29,7 +42,7 @@ void setup() {
   myservo.attach(SERVO_PIN);
   
   // Initialize the PS2 controller
-  ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT);
+  ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT,true,true); //setup pins and settings:  GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
   
   // Set motor pins as outputs
   pinMode(M1, OUTPUT);
@@ -43,10 +56,10 @@ void loop() {
 
     // Circle button - move forward
     if (ps2x.ButtonPressed(PSB_CIRCLE)) {
-      digitalWrite(M1, HIGH);
-      digitalWrite(M2, HIGH);
-      analogWrite(M1, leftspeed);
-      analogWrite(M2, rightspeed);
+      digitalWrite(M1, HIGH); // Set motor 1 to forward
+      digitalWrite(M2, HIGH); // Set motor 2 to forward
+      analogWrite(M1, leftspeed); // Set motor 1 speed
+      analogWrite(M2, rightspeed); // Set motor 2 speed
       Serial.println("Circle pressed - Forward");
     }
 
@@ -79,11 +92,11 @@ void loop() {
 
     // L1 or R1 buttons - activate servo motor
     if (ps2x.ButtonPressed(PSB_L1) || ps2x.ButtonPressed(PSB_R1)) {
-      myservo.write(180);
+      myservo.write(180); // Set servo to 180 degrees
       Serial.println("L1 button pressed");
     }
     else {
-      myservo.write(0);
+      myservo.write(0); // Set servo to 0 degrees
     }
   }
 }
